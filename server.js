96dba57d6858825232ec1d8171cce088c7186f21
Eli Makston <73192122+EliMakston@users.noreply.github.com>
2023-01-18 20:31:03 +0000

    changed falsy to null

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const PORT = 4000;
 
 app.use(morgan('tiny'));
-
+app.use(bodyParser.json());
 app.use(express.static('public'));
 
 class User {
@@ -23,11 +24,16 @@ app.listen(PORT, () => {
 });
 
 app.post('/newUser', (req, res, next) => {
-    const newUser = new User(userList.length+1);
+    const newUser = new User(userList.length);
     userList.push(newUser);
     console.log(userList);
     res.status(201).send(userList[userList.length - 1]);
 });
+
+app.post('/choice', (req, res, next) => {
+    const userId = req.body.userId;
+    const choice = req.body.choice;
+})
 
 app.get('/', (req, res, next) => {
     res.sendFile('client.html', {root: __dirname });
