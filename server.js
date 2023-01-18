ff1099f64cf3054ed4bdfb583270441e8c11c1e3
Eli Makston <73192122+EliMakston@users.noreply.github.com>
2023-01-18 19:50:19 +0000

    newUser now working

--- server.js
+++ server.js
@@ -1,8 +1,28 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const PORT = 4000;
 
+app.use(morgan('tiny'));
+
+class User {
+    constructor(id) {
+        this._id = id;
+        this.choice = undefined;
+        this.hasChosen = false;
+    }
+}
+
+const userList = [];
+
 app.listen(PORT, () => {
     console.log(`Now listening on port ${PORT}`);
+});
+
+app.post('/newUser', (req, res, next) => {
+    const newUser = new User(userList.length+1);
+    userList.push(newUser);
+    console.log(userList);
+    res.status(201).send(userList[userList.length - 1]);
 });
